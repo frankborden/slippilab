d3a5653508cdf8a9bd46138d46d9369d51701309
Frank Borden <frankborden2@gmail.com>
2021-07-16 02:33:35 +0200

    cleanup animation loading

--- src/viewer/animations/index.ts
+++ src/viewer/animations/index.ts
@@ -4,9 +4,34 @@ export { actions, specials } from './actions';
 // Adapted directly from https://github.com/vinceau/react-slp-viewer
 
 // Store all the imported animations here
-const animationMap = new Map<number, any>();
+const loadedAnimationsByCharacterId = new Map<number, any>();
 
-const importAnimation = async (
+export type AnimationFrames = AnimationFrame[];
+export type AnimationFrame = Int16Array;
+export type RawCharacterAnimations = { [actionName: string]: number[][][] };
+export type CharacterAnimations = { [actionName: string]: AnimationFrames };
+
+export const fetchAnimation = async (
+  charId: number,
+): Promise<CharacterAnimations> => {
+  if (loadedAnimationsByCharacterId.has(charId)) {
+    return loadedAnimationsByCharacterId.get(charId);
+  }
+
+  const rawAnimations = await importRawAnimation(charId);
+  const animations: CharacterAnimations = {};
+  Object.entries(rawAnimations).forEach(([name, data]) => {
+    animations[name] = data.map((frame) => {
+      return new Int16Array(frame[0]);
+    });
+  });
+
+  // Store it for later
+  loadedAnimationsByCharacterId.set(charId, animations);
+  return animations;
+};
+
+const importRawAnimation = async (
   charId: number,
 ): Promise<RawCharacterAnimations> => {
   switch (characterNamesById[charId]) {
@@ -29,29 +54,4 @@ const importAnimation = async (
       throw new Error(`Unsupported character id: ${charId}`);
     }
   }
-};
-
-export type AnimationFrames = AnimationFrame[];
-export type AnimationFrame = Int16Array;
-export type RawCharacterAnimations = { [actionName: string]: number[][][] };
-export type CharacterAnimations = { [actionName: string]: AnimationFrames };
-
-export const fetchAnimation = async (
-  charId: number,
-): Promise<CharacterAnimations> => {
-  if (animationMap.has(charId)) {
-    return animationMap.get(charId);
-  }
-
-  const charAnimations = await importAnimation(charId);
-  const converted: CharacterAnimations = {};
-  Object.entries(charAnimations).forEach(([actionName, actionData]) => {
-    converted[actionName] = actionData.map((innerData) => {
-      return new Int16Array(innerData[0]);
-    });
-  });
-
-  // Store it for later
-  animationMap.set(charId, converted);
-  return converted;
 };
