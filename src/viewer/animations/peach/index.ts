743918cb4de1f8f9ef3bc20bc9ba145754ca4c14
Frank Borden <frankborden2@gmail.com>
2021-08-01 17:32:41 +0200

    cleanup unzipping

--- src/viewer/animations/peach/index.ts
+++ src/viewer/animations/peach/index.ts
@@ -3,17 +3,16 @@ import * as zip from '@zip.js/zip.js';
 
 export let animations: { [animationName: string]: string[] } = {};
 (async () => {
-  const animationsZip = await fetch(animationsUrl).then((r) => r.blob());
+  const response = await fetch(animationsUrl);
+  const animationsZip = await response.blob();
   const reader = new zip.ZipReader(new zip.BlobReader(animationsZip));
   const files = await reader.getEntries();
-  await Promise.all(
-    files.map(async (file) => {
-      const animationName = file.filename.replace('.json', '');
-      const contents = await file.getData?.(new zip.TextWriter());
-      const animationData = JSON.parse(contents);
-      animations[animationName] = animationData;
-    }),
-  );
+  files.forEach(async (file) => {
+    const animationName = file.filename.replace('.json', '');
+    const contents = await file.getData?.(new zip.TextWriter());
+    const animationData = JSON.parse(contents);
+    animations[animationName] = animationData;
+  });
 })();
 
 export default animations;
