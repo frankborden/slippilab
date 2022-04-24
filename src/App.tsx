8be681773465fd87bb0aea7aa593d0aa33ce7c04
Frank Borden <frankborden2@gmail.com>
2022-04-24 23:34:17 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <svg viewBox="0 0 200 200">
+      <circle
+        cx="50"
+        cy="50"
+        r="40"
+        stroke="black"
+        stroke-width="3"
+        fill="red"
+      />
+    </svg>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+});
