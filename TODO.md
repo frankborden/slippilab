ce78238577a12e5f73610e8782b38c596fbf1508
Frank Borden <frankborden2@gmail.com>
2022-05-07 23:24:17 +0200

    displayname cascade, stock count circles

--- TODO.md
+++ TODO.md
@@ -5,7 +5,6 @@ For feature parity:
 - URL support
 - Clip stuff
 - Search stuff
-- Display name cascade
 - Timer
 - Nana
 - Teams colors
--- src/viewer/PlayerHUD.tsx
+++ src/viewer/PlayerHUD.tsx
@@ -1,10 +1,13 @@
-import { createMemo } from "solid-js";
+import { createMemo, For } from "solid-js";
 import { characterNameByInternalId } from "../common/ids";
 import { state } from "../state";
 
 export function PlayerHUD(props: { player: number }) {
   const playerState = createMemo(
     () => state.replayData()!.frames[state.frame()].players[props.player].state
+  );
+  const playerSettings = createMemo(
+    () => state.replayData()!.settings.playerSettings[props.player]
   );
   const position = createMemo(() => ({
     x: -30 + 20 * props.player, // ports at: -30%, -10%, 10%, 30%
@@ -13,22 +16,32 @@ export function PlayerHUD(props: { player: number }) {
   const color = createMemo(
     () => ["darkred", "darkblue", "gold", "darkgreen"][props.player]
   );
+  const name = createMemo(() =>
+    [
+      playerSettings().displayName,
+      playerSettings().connectCode,
+      playerSettings().nametag,
+      playerSettings().playerType === 1
+        ? "CPU"
+        : characterNameByInternalId[playerState().internalCharacterId],
+    ].find((n) => n?.length > 0)
+  );
   return (
     <>
+      <For each={Array(playerState().stocksRemaining).fill(0)}>
+        {(_, i) => (
+          <circle
+            cx={`${position().x - 2 * (1.5 - i())}%`}
+            cy={`-${position().y}%`}
+            r={5}
+            fill={color()}
+          />
+        )}
+      </For>
       <text
         style={{ font: "bold 15px sans-serif", transform: "scaleY(-1)" }}
         x={`${position().x}%`}
-        y={`${position().y}%`}
-        text-anchor="middle"
-        textContent={`${Math.floor(
-          playerState().stocksRemaining
-        )} stocks remaining`}
-        fill={color()}
-      />
-      <text
-        style={{ font: "bold 15px sans-serif", transform: "scaleY(-1)" }}
-        x={`${position().x}%`}
-        y={`${position().y + 3}%`}
+        y={`${position().y + 4}%`}
         text-anchor="middle"
         textContent={`${Math.floor(playerState().percent)}%`}
         fill={color()}
@@ -36,12 +49,9 @@ export function PlayerHUD(props: { player: number }) {
       <text
         style={{ font: "bold 15px sans-serif", transform: "scaleY(-1)" }}
         x={`${position().x}%`}
-        y={`${position().y + 6}%`}
+        y={`${position().y + 7}%`}
         text-anchor="middle"
-        textContent={
-          // TODO: Slippi name, Slippi code, In-game tag, CPU
-          characterNameByInternalId[playerState().internalCharacterId]
-        }
+        textContent={name()}
         fill={color()}
       />
     </>
