8057c0d464059a165a7fd49f9d159ff9aba2a24a
Frank Borden <frankborden2@gmail.com>
2022-05-19 01:30:03 +0200

    match melee's hundredths

--- src/viewer/Timer.tsx
+++ src/viewer/Timer.tsx
@@ -4,6 +4,68 @@ import { state } from "../state";
 
 export function Timer() {
   const color = useColorModeValue("black", "#dddddd");
+  const meleeHundredths = [
+    "00",
+    "02",
+    "04",
+    "06",
+    "07",
+    "09",
+    "11",
+    "12",
+    "14",
+    "16",
+    "17",
+    "19",
+    "21",
+    "22",
+    "24",
+    "26",
+    "27",
+    "29",
+    "31",
+    "32",
+    "34",
+    "36",
+    "37",
+    "39",
+    "41",
+    "42",
+    "44",
+    "46",
+    "47",
+    "49",
+    "51",
+    "53",
+    "54",
+    "56",
+    "58",
+    "59",
+    "61",
+    "63",
+    "64",
+    "66",
+    "68",
+    "69",
+    "71",
+    "73",
+    "74",
+    "76",
+    "78",
+    "79",
+    "81",
+    "83",
+    "84",
+    "86",
+    "88",
+    "89",
+    "91",
+    "93",
+    "94",
+    "96",
+    "98",
+    "99",
+  ];
   const time = createMemo(() => {
     const frames =
       state.replayData()!.settings.timerStart * 60 - state.frame() + 123;
@@ -13,8 +75,7 @@ export function Timer() {
     const seconds = Math.floor((frames % (60 * 60)) / 60)
       .toString()
       .padStart(2, "0");
-    // TODO match Melee's frame -> hundredths calculation
-    const hundredths = ((frames % 60) / 60).toFixed(2).slice(2);
+    const hundredths = meleeHundredths[frames % 60];
     return `${minutes}:${seconds}:${hundredths}`;
   });
   return (
