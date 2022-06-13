949fe052c584cde04402b8be09fdbea993167d79
Frank Borden <53030687+frankborden@users.noreply.github.com>
2022-06-13 21:46:12 +0000

    de-hope table

--- src/sidebar/SettingsTab.tsx
+++ src/sidebar/SettingsTab.tsx
@@ -1,122 +1,113 @@
-import {
-  Kbd,
-  Table,
-  TableCaption,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@hope-ui/solid";
+import { Kbd } from "@hope-ui/solid";
 
 export function SettingsTab() {
   return (
     <>
       <div class="flex flex-col items-center gap-2 overflow-y-auto">
-        <Table>
-          <TableCaption placement="top">Playback Shortcuts</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Shortcut</Th>
-              <Th>Description</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>
+        <table>
+          <caption>Playback Shortcuts</caption>
+          <thead>
+            <tr>
+              <th>Shortcut</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
                 <Kbd>space</Kbd>/<Kbd>K</Kbd>
-              </Td>
-              <Td>Toggle pause</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Toggle pause</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>LeftArrow</Kbd>/<Kbd>J</Kbd>
-              </Td>
-              <Td>Rewind 2 seconds</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Rewind 2 seconds</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>RightArrow</Kbd>/<Kbd>L</Kbd>
-              </Td>
-              <Td>Skip ahead 2 seconds</Td>
-            </Tr>
+              </td>
+              <td>Skip ahead 2 seconds</td>
+            </tr>
 
-            <Tr>
-              <Td>
+            <tr>
+              <td>
                 <Kbd>0</Kbd>-<Kbd>9</Kbd>
-              </Td>
-              <Td>
+              </td>
+              <td>
                 Jump to xx%. For example, press <Kbd>3</Kbd> for 30%
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>.</Kbd>
-              </Td>
-              <Td>Next frame (pauses if not paused)</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Next frame (pauses if not paused)</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>,</Kbd>
-              </Td>
-              <Td>Previous frame (pauses if not paused)</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Previous frame (pauses if not paused)</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>UpArrow</Kbd>
-              </Td>
-              <Td>Slow speed</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Slow speed</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>DownArrow</Kbd>
-              </Td>
-              <Td>Fast speed</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Fast speed</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>+</Kbd>/<Kbd>=</Kbd>
-              </Td>
-              <Td>Zoom in</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Zoom in</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>-</Kbd>/<Kbd>_</Kbd>
-              </Td>
-              <Td>Zoom out</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Zoom out</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>[</Kbd>/<Kbd>{"{"}</Kbd>
-              </Td>
-              <Td>Play previous file</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Play previous file</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>]</Kbd>/<Kbd>{"}"}</Kbd>
-              </Td>
-              <Td>Play next file</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Play next file</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>;</Kbd>/<Kbd>:</Kbd>
-              </Td>
-              <Td>Play previous clip</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Play previous clip</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>'</Kbd>/<Kbd>"</Kbd>
-              </Td>
-              <Td>Play next clip</Td>
-            </Tr>
-            <Tr>
-              <Td>
+              </td>
+              <td>Play next clip</td>
+            </tr>
+            <tr>
+              <td>
                 <Kbd>d</Kbd>
-              </Td>
-              <Td>Toggle debug output</Td>
-            </Tr>
-          </Tbody>
-        </Table>
+              </td>
+              <td>Toggle debug output</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
