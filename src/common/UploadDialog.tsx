2cf419bc783ba0d7c5cc44176d97f0924fa402f8
Frank Borden <53030687+frankborden@users.noreply.github.com>
2022-06-14 19:34:12 +0000

    zagjs dialog

---
+++ src/common/UploadDialog.tsx
@@ -0,0 +1,109 @@
+import * as dialog from "@zag-js/dialog";
+import { Portal } from "solid-js/web";
+import { useMachine, useSetup, normalizeProps } from "@zag-js/solid";
+import { createEffect, createMemo, createSignal, on, Show } from "solid-js";
+import { Button } from "./Button";
+import { store } from "../state";
+import { uploadReplay } from "../supabaseClient";
+import { Spinner } from "@hope-ui/solid";
+
+export function UploadDialog() {
+  const [dialogState, dialogSend] = useMachine(dialog.machine);
+  const dialogRef = useSetup({ send: dialogSend, id: "uploadDialog" });
+  const dialogApi = createMemo(() =>
+    dialog.connect(dialogState, dialogSend, normalizeProps)
+  );
+  const isDialogOpen = createMemo(() => dialogApi().isOpen);
+  const isDialogClosed = createMemo(() => !dialogApi().isOpen);
+
+  const [isUploading, setIsUploading] = createSignal(false);
+  const [url, setUrl] = createSignal<string | undefined>();
+  const [error, setError] = createSignal<string | undefined>();
+  const [isUrlCopied, setIsUrlCopied] = createSignal(false);
+
+  createEffect(
+    on(
+      isDialogOpen,
+      async () => {
+        setIsUploading(true);
+        const file = store.files[store.currentFile];
+        const { id, data, error } = await uploadReplay(file);
+        if (data != null) {
+          setUrl(`${window.location.origin}/${id}`);
+        } else {
+          setError("Error uploading file");
+          console.error(error);
+        }
+        setIsUploading(false);
+      },
+      { defer: true }
+    )
+  );
+
+  createEffect(
+    on(isDialogClosed, () => {
+      setIsUrlCopied(false);
+      setUrl();
+      setError();
+    })
+  );
+
+  return (
+    <>
+      <Button ref={dialogRef} {...dialogApi().triggerProps}>
+        Upload
+      </Button>
+      {dialogApi().isOpen && (
+        <Portal>
+          {/* @ts-ignore */}
+          <div
+            {...dialogApi().backdropProps}
+            class="absolute top-0 w-screen h-screen left-0 bg-slate-800 opacity-25"
+          />
+          {/* @ts-ignore */}
+          <div
+            {...dialogApi().underlayProps}
+            class="absolute top-0 left-0 flex w-full h-full justify-center items-center"
+          >
+            {/* @ts-ignore */}
+            <div
+              {...dialogApi().contentProps}
+              class="bg-slate-50 border border-slate-700 rounded w-6 p-4"
+            >
+              {/* @ts-ignore */}
+              <h1 {...dialogApi().titleProps} class="text-lg">
+                Replay Upload
+              </h1>
+              {/* @ts-ignore */}
+              <div {...dialogApi().descriptionProps}>
+                <div class="flex gap-2 justify-center items-center">
+                  <Show when={!isUploading()} fallback={<Spinner />}>
+                    <Show when={url()} fallback={error()}>
+                      <code class="text-sm">{url()}</code>
+                      <Button
+                        class="material-icons cursor-pointer text-lg px-1 py-0"
+                        onClick={() => {
+                          const link = url();
+                          if (link === undefined) return;
+                          void navigator.clipboard.writeText(link);
+                          setIsUrlCopied(true);
+                        }}
+                      >
+                        content_copy
+                      </Button>
+                      <Show when={isUrlCopied()}>Copied!</Show>
+                    </Show>
+                  </Show>
+                </div>
+              </div>
+              <div class="flex justify-end">
+                {/* @ts-ignore */}
+                <Button {...dialogApi().closeButtonProps}>Close</Button>
+              </div>
+            </div>
+          </div>
+        </Portal>
+      )}
+    </>
+  );
+}
--- src/sidebar/Upload.tsx
+++ src/sidebar/Upload.tsx
@@ -1,130 +1,15 @@
-import { load, store } from "../state";
-import { Button } from "../common/Button";
-import {
-  createDisclosure,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  Spinner,
-  notificationService,
-} from "@hope-ui/solid";
-import { createSignal, Show } from "solid-js";
-import { uploadReplay } from "../supabaseClient";
-import { filterFiles } from "../common/util";
+import { store } from "../state";
+import { Show } from "solid-js";
 import { OpenMenu } from "../common/OpenMenu";
+import { UploadDialog } from "../common/UploadDialog";
 
 export function Upload() {
-  const { isOpen, onOpen, onClose } = createDisclosure();
-  const [isUploading, setIsUploading] = createSignal(false);
-  const [url, setUrl] = createSignal<string | undefined>();
-  const [error, setError] = createSignal<string | undefined>();
-
-  let fileInput!: HTMLInputElement;
-  let folderInput!: HTMLInputElement;
-
-  async function onFileSelected(e: Event): Promise<void> {
-    const input = e.target as HTMLInputElement;
-
-    if (input.files === null || input.files.length === 0) {
-      return;
-    }
-    const files = Array.from(input.files);
-    const filteredFiles = await filterFiles(files);
-    return await load(filteredFiles);
-  }
-
-  async function onUpload(): Promise<void> {
-    setIsUploading(true);
-    onOpen();
-    const file = store.files[store.currentFile];
-    const { id, data, error } = await uploadReplay(file);
-    if (data != null) {
-      setUrl(`${window.location.origin}/${id}`);
-    } else {
-      setError("Error uploading file");
-      console.error(error);
-    }
-    setIsUploading(false);
-  }
-
   return (
-    <>
-      <div class="flex w-full items-center justify-between gap-2">
-        <OpenMenu name={"Open"} />
-        <Show when={store.files.length > 0}>
-          <Button class="whitespace-normal py-1" onClick={onUpload}>
-            <div class="flex items-center gap-2">
-              Upload
-              <div class="material-icons-outlined" aria-label="Upload File">
-                cloud_upload
-              </div>
-            </div>
-          </Button>
-        </Show>
-        <Modal opened={isOpen()} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Replay Upload</ModalHeader>
-            <ModalBody>
-              <div class="flex items-center justify-center">
-                <Show when={!isUploading()} fallback={<Spinner />}>
-                  <Show
-                    when={url() !== undefined}
-                    fallback={<div>{error()}</div>}
-                  >
-                    <div class="flex items-center gap-4">
-                      <a href={url()}>{url()}</a>
-                      <div
-                        class="cursor-pointer"
-                        onClick={() => {
-                          const link = url();
-                          if (link === undefined) return;
-                          void navigator.clipboard.writeText(link);
-                          notificationService.show({
-                            status: "success",
-                            duration: 1000,
-                            title: "Link copied",
-                          });
-                        }}
-                      >
-                        <div
-                          class="material-icons cursor-pointer text-4xl"
-                          aria-label="Copy URL"
-                        >
-                          content_copy
-                        </div>
-                      </div>
-                    </div>
-                  </Show>
-                </Show>
-              </div>
-            </ModalBody>
-            <ModalFooter>
-              <Button onClick={onClose}>Close</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-        <input
-          class="hidden"
-          type="file"
-          accept=".slp,.zip"
-          multiple
-          ref={fileInput}
-          onChange={onFileSelected}
-        />
-        <input
-          class="hidden"
-          type="file"
-          // @ts-expect-error folder input is not standard, but is supported by all
-          // modern browsers
-          webkitDirectory
-          ref={folderInput}
-          onChange={onFileSelected}
-        />
-      </div>
-    </>
+    <div class="flex w-full items-center justify-between">
+      <OpenMenu name={"Open"} />
+      <Show when={store.files.length > 0}>
+        <UploadDialog />
+      </Show>
+    </div>
   );
 }
