558dd187f0a42c31c374611a8fd73883afbe8299
Frank Borden <53030687+frankborden@users.noreply.github.com>
2022-07-16 21:33:58 +0000

    vitest experiment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
-import viteTsconfigPaths from 'vite-tsconfig-paths';
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   assetsInclude: /.*zip$/,
-  plugins: [solidPlugin(), viteTsconfigPaths()]
-})
+  plugins: [solidPlugin(), viteTsconfigPaths()],
+  test: {
+    environment: "jsdom",
+    transformMode: {
+      web: [/.[jt]sx?/],
+    },
+    deps: {
+      inline: [/solid-js/, /solid-testing-library/],
+    },
+  },
+});
