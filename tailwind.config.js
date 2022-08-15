7ac261518f56349b6b178cfd6ca6478ab912d27b
Frank Borden <53030687+frankborden@users.noreply.github.com>
2022-08-15 09:29:54 +0000

    animate landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slippi: {
+          50: "#BFE5CB",
+          100: "#B1DFBF",
+          200: "#94D4A7",
+          300: "#77C890",
+          400: "#59BC78",
+          500: "#44A963",
+          600: "#34814C",
+          700: "#245934",
+          800: "#14311D",
+          900: "#040905",
+        },
+      },
+      animation: {
+        draw: "draw 6s ease-in-out forwards",
+      },
+      keyframes: {
+        draw: {
+          to: { "stroke-dashoffset": 50 },
+        },
+      },
+    },
   },
   plugins: [],
 };
