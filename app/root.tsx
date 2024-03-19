f0165ceaa14d4aaed83d33ac7a27c9d0f64c34d5
Frank Borden <frankborden2@gmail.com>
2024-03-20 00:04:54 +0100

    init

---
+++ app/root.tsx
@@ -0,0 +1,42 @@
+import { MetaFunction } from "@remix-run/cloudflare";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import "~/tailwind.css";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Slippilab" },
+    {
+      name: "description",
+      content: "Watch Slippi replays",
+    },
+  ];
+};
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
---
+++ app/routes/_index.tsx
@@ -0,0 +1,7 @@
+import { Button } from "~/components/ui/button";
+
+export default function Page() {
+  return (
+    <Button>Start</Button>
+  );
+}
