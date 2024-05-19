da9a2772280fc591b626c056f4f7c0bb8cc80b5e
Frank Borden <frankborden2@gmail.com>
2024-05-19 20:00:30 +0200

    cleanup

--- packages/www/app/routes/_index.tsx
+++ packages/www/app/routes/_index.tsx
@@ -1,7 +1,8 @@
 export default function Page() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main>
+      <div className="text-4xl">Hello World</div>
+      <div className="i-tabler-user-circle size-12" />
+    </main>
   );
 }
